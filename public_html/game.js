1c8c7384ce2a60ca6963ebbfd5ab106e742d4bf2
jermaine <jaylarcane@gmail.com>
2017-02-10 21:22:16 -0500

    binding variables

--- public_html/game.js
+++ public_html/game.js
@@ -1,5 +1,8 @@
 var Game = function (game) {
     this.game = game;
+    
+    // All game variables will be binded to the Game object.
+    var logo; // Okay, okay... This is how we're going to do it!--Honestly, I don't get it!
 };
 
 Game.prototype = {
@@ -8,7 +11,7 @@ Game.prototype = {
     },
 
     create: function () {
-        var logo = this.add.sprite(this.world.centerX, this.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+        this.logo = this.add.sprite(this.world.centerX, this.world.centerY, 'logo'); // i think 'var logo;' binded it to Game
+        this.logo.anchor.setTo(0.5, 0.5);
     }
 };
