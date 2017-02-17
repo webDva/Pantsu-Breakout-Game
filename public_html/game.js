a9b48465278a7833bcba5e9fa93158110304d49a
jermaine <jaylarcane@gmail.com>
2017-02-16 20:16:29 -0500

    scaled pantsu image

--- public_html/game.js
+++ public_html/game.js
@@ -2,16 +2,18 @@ var Game = function (game) {
     var game = game;
     
     // All game variables will be binded to the Game object.
-    var logo; // Okay, okay... This is how we're going to do it!--Honestly, I don't get it!
+    var pantsu;
 };
 
 Game.prototype = {
     preload: function () {
-        this.load.image('logo', 'assets/phaser.png');
+        this.load.image('pantsu', 'assets/pantsu.png');
     },
 
     create: function () {
-        this.logo = this.add.sprite(this.world.centerX, this.world.centerY, 'logo'); // i think 'var logo;' binded it to Game
-        this.logo.anchor.setTo(0.5, 0.5);
+        this.pantsu = this.add.sprite(this.world.centerX, this.world.centerY, 'pantsu');
+        this.pantsu.anchor.setTo(0.5, 0.5);
+        
+        this.pantsu.scale.setTo(0.3, 0.3);
     }
 };
