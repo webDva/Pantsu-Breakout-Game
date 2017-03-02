9f47d2b4046ff2648512b3da45567c348eea9a26
jermaine <jaylarcane@gmail.com>
2017-03-01 20:16:23 -0500

    created 2D array of pantsu

--- public_html/game.js
+++ public_html/game.js
@@ -2,9 +2,12 @@ var Game = function (game) {
     var game = game;
     
     // All game variables will be binded to the Game object.
-    var pantsu;
     var pantsuGroup;
-    var ROWS, COLUMNS;
+    
+    ROWS = 10;
+    COLUMNS = 18;
+    
+    SCALE = 32; // May not be responsive-friendly doing it like this
 };
 
 Game.prototype = {
@@ -20,15 +23,13 @@ Game.prototype = {
         this.pantsuGroup.enableBody = true;
         
         /* Create 2D array of pantsu */
-        for (i = 0; i < this.ROWS; ++i) {
-            for (j = 0; j < this.COLUMNS; ++j) {
-                
+        for (i = 0; i < COLUMNS; i++) {
+            for (j = 0; j < ROWS; j++) {
+                this.pantsuGroup.create(i * SCALE, j * SCALE, 'pantsu');
             }
         }
         
-        this.pantsu = this.add.sprite(this.world.centerX, this.world.centerY, 'pantsu');
-        this.pantsu.anchor.setTo(0.5, 0.5);
-        
-        this.pantsu.scale.setTo(0.3, 0.3);
+        this.pantsuGroup.setAll('width', SCALE);
+        this.pantsuGroup.setAll('height', SCALE);
     }
 };
